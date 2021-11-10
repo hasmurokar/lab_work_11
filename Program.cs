21f85b40c5d74419d5d028b94658500c094be887
margaritakaiki@outlook.com <margaritakaiki@outlook.com>
2021-11-10 21:12:05 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lab_work_11
+{
+    class Program
+    { 
+        private static Random randomizer = new Random();
+        static void Main(string[] args)
+        {
+            var triangleArray = new Triangle[8];
+            for (int i = 0; i < triangleArray.Length; i++)
+            {
+                while (true)
+                {
+                    var angleOne = randomizer.Next(30, 120);
+                    var angleTwo = randomizer.Next(30, 120);
+                    if (angleOne + angleTwo < 180)
+                    {
+                        var angleThree = 180 - (angleOne + angleTwo);
+                        triangleArray[i] = new Triangle(angleOne, angleTwo, angleThree);
+                        break;
+                    }
+                }
+                
+            }
+            foreach (var item in triangleArray)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Кол-во остроуг. = {Triangle.counterSharp}");
+            foreach (var item in triangleArray)
+            {
+                if (item.SharpTriangle())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine($"Кол-во прямоуг. = {Triangle.counterRect}");
+            foreach (var item in triangleArray)
+            {
+                if (item.RectTriangle())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine($"Кол-во тупоуг. = {Triangle.counterUnsharp}");
+            foreach (var item in triangleArray)
+            {
+                if (item.UnsharpTriangle())
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_work_11
+{
+    class Triangle
+    {
+        public int AngleOne { get; set; }
+        public int AngleTwo { get; set; }
+        public int AngleThree { get; set; }
+        public static int counterSharp { get; set; }
+        public static int counterRect { get; set; }
+        public static int counterUnsharp { get; set; }
+
+        public Triangle(int angleOne, int angleTwo, int angleThree)
+        {
+            AngleOne = angleOne;
+            AngleTwo = angleTwo;
+            AngleThree = angleThree;
+            Count();
+        }
+
+        public void Count()
+        {
+            if (AngleOne == 90 || AngleTwo == 90 || AngleThree == 90)
+            {
+                counterRect++;
+            }
+            else if (AngleOne < 90 && AngleTwo < 90 && AngleThree < 90)
+            {
+                counterSharp++;
+            }
+            else
+            {
+                counterUnsharp++;
+            }
+        }
+        public bool SharpTriangle()
+        {
+            if (AngleOne < 90 && AngleTwo < 90 && AngleThree < 90)
+            {
+                return true;
+            }
+            else return false;
+            
+        }
+
+        public bool RectTriangle()
+        {
+            if (AngleOne == 90 || AngleTwo == 90 || AngleThree == 90)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool UnsharpTriangle()
+        {
+            if (!SharpTriangle() && !RectTriangle())
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public override string ToString()
+        {
+            return $"{AngleOne}, {AngleTwo}, {AngleThree}";
+        }
+    }
+}
